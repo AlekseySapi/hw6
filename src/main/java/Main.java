eab294bc22ad6b3d72ac74ae738c292b86b004ca
Aleksey L <52339899+AlekseySapi@users.noreply.github.com>
2021-06-18 12:32:58 +0300

    6.5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -74,14 +74,6 @@ class Tree {
 
     //        Задание 6.4
 
-    private void inOrder(Node rootNode) {
-        if (rootNode != null) {
-            inOrder(rootNode.leftChild);
-            rootNode.display();
-            inOrder(rootNode.rightChild);
-        }
-    }
-
     public Node min() {
         Node current, last = null;
         current = root;
@@ -104,8 +96,119 @@ class Tree {
         return last;
     }
 
+    private void preOrder(Node rootNode) {
+        if (rootNode != null) {
+            rootNode.display();
+            preOrder(rootNode.leftChild);
+            preOrder(rootNode.rightChild);
+        }
+    }
 
+    private void postOrder(Node rootNode) {
+        if (rootNode != null) {
+            postOrder(rootNode.leftChild);
+            postOrder(rootNode.rightChild);
+            rootNode.display();
+        }
+    }
+
+    private void inOrder(Node rootNode) {
+        if (rootNode != null) {
+            inOrder(rootNode.leftChild);
+            rootNode.display();
+            inOrder(rootNode.rightChild);
+        }
+    }
+
+
+    public boolean delete(int id) {
+
+        Node current = root;
+        Node parent = root;
+
+        boolean isLeftChild = true;
+
+        while (current.person.id != id) {
+            parent = current;
+            if (id < current.person.id) {
+                isLeftChild = true;
+                current = current.leftChild;
+            } else {
+                isLeftChild = false;
+                current = current.rightChild;
+            }
+            if (current == null) {
+                return false;
+            }
+        }
+        if (current.leftChild == null && current.rightChild == null) {
+            if (current == root) {
+                root = null;
+            } else if (isLeftChild) {
+                parent.leftChild = null;
+            } else {
+                parent.rightChild = null;
+            }
+        } else if (current.rightChild == null) {
+            if (current == root) {
+                root = current.leftChild;
+            } else if (isLeftChild) {
+                parent.leftChild = current.leftChild;
+            } else {
+                parent.rightChild = current.leftChild;
+            }
+        } else if (current.leftChild == null) {
+            if (current == root) {
+                root = current.rightChild;
+            } else if (isLeftChild) {
+                parent.leftChild = current.rightChild;
+            } else {
+                parent.rightChild = current.rightChild;
+            }
+        } else {
+            Node successor = getSuccesor(current);
+            if (current == root) {
+                root = successor;
+            } else if (isLeftChild) {
+                parent.leftChild = successor;
+            } else {
+                parent.rightChild = successor;
+            }
+            successor.leftChild = current.leftChild;
+        }
+        return true;
+    }
+
+    public Node getSuccesor(Node node) {
+        Node successorParent = node;
+        Node successor = node;
+        Node current = node.rightChild;
+
+
+        while (current != null) {
+            successorParent = successor;
+            successor = current;
+            current = current.leftChild;
+        }
+        if (successor != node.rightChild) {
+            successorParent.leftChild = successor.rightChild;
+            successor.rightChild = node.rightChild;
+        }
+
+        return successor;
+    }
+
+    public void displayTree() {
+        Node current = root;
+        System.out.println("Симметричный");
+        inOrder(root);
+        System.out.println("Прямой");
+        preOrder(root);
+        System.out.println("Обратный");
+        postOrder(current);
+    }
 }
+
 
 public class Main {
 
@@ -117,7 +220,7 @@ public class Main {
         Быстрая вставка, поиск и удаление элемента
  */
 
-//        Реализация:
+//        Реализация (6.2, 6.3):
 
         Tree myTree = new Tree();
 
@@ -134,6 +237,18 @@ public class Main {
         myTree.find(3).display();
 
         System.out.println();
+
+
+//        Задание 6.5
+
+        long startTime = System.nanoTime();
+        myTree.delete(5);
+        long endTime = System.nanoTime();
+        System.out.println("Удаление заняло: " + (endTime - startTime) + " нс");
+
+        System.out.println();
+
+        myTree.displayTree();
 
 
     }
