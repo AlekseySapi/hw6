82a40446e735704fdd87db73696a5a3b6dd1618f
Aleksey L <52339899+AlekseySapi@users.noreply.github.com>
2021-06-18 12:04:33 +0300

    6.2, 6.3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,79 @@
+class Person {
+    public String name;
+    public int id;
+    public int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+}
+
+
+//        Задание 6.2
+
+class Node {
+    public Person person;
+    public Node leftChild;
+    public Node rightChild;
+
+    public void display() {
+        System.out.println("Имя: " + person.name + " Возраст: " + person.age);
+    }
+}
+
+
+class Tree {
+    private Node root;
+
+    //        Задание 6.3
+
+    public void insert(Person person) {
+        Node node = new Node();
+        node.person = person;
+        if (root == null) {
+            root = node;
+        } else {
+            Node current = root;
+            Node parent;
+            while (true) {
+                parent = current;
+                if (person.id < current.person.id) {
+                    current = current.leftChild;
+                    if (current == null) {
+                        parent.leftChild = node;
+                        return;
+                    }
+                } else {
+                    current = current.rightChild;
+                    if (current == null) {
+                        parent.rightChild = node;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public Node find(int key) {
+        Node current = root;
+        while (current.person.id != key) {
+            if (key < current.person.id) {
+                current = current.leftChild;
+            } else {
+                current = current.rightChild;
+            }
+            if (current == null) {
+                return null;
+            }
+        }
+        return current;
+    }
+
+
+}
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,8 +83,6 @@ public class Main {
         Пример использования древовидной структуры:
         Быстрая вставка, поиск и удаление элемента
  */
-
-
 
 
     }
